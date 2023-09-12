db7b23213c30440a8732c554204e485b6105d004
Kittiya <angiekittiyabusiness@gmail.com>
2023-09-13 00:30:08 +0700

    Updated project collaspe

--- pjfinal/src/components/dashboard.js
+++ pjfinal/src/components/dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './maindashboard.css';
-import { Card,Collapse,ConfigProvider,Button} from 'antd';
-import { PlusOutlined,FormOutlined,DeleteOutlined } from '@ant-design/icons';
+import { Card,theme,Collapse,ConfigProvider,Button} from 'antd';
+import { PlusOutlined,RightOutlined,CloseOutlined,FormOutlined } from '@ant-design/icons';
 import { Link} from "react-router-dom";
 import axios from "axios";
 import { useState,useEffect } from "react";
@@ -10,7 +10,8 @@ const Dashboard = () => {
     const user = localStorage.user
     const [projectdata, setProjectData] = useState([]);
     const [deletee,setDelete] = useState([]);
-
+    
+  
     useEffect(() => {
         axios.get(`http://127.0.0.1:5000/home?user=${user}`)
             .then(response => {
@@ -46,38 +47,40 @@ const Dashboard = () => {
                 {projectdata.map((project, index) => (
 
                   //<Link to={`/myproject/${project[1]}/${project[2]}`}>details</Link>
-                  //onClick={()=>Deleteprojuct(project[2])}
+                  
                   //
                   <div className="projindash">
-                    <ConfigProvider
-                        theme={{
-                          token: {
-                            contentBg: '#AFDBDA' ,
-                          },
-                        }}
-                      >
+                   
                     <Collapse className="projcollaspe"
                       collapsible="header"
                       size="small"
                       defaultActiveKey={['1']}
+                      expandIcon={({ isActive }) => <RightOutlined className="projcollaspe-ico" rotate={isActive ? 90 : 0} style={{ fontSize: '16px',marginTop:'40px'}} />}
                       items={[
                       {
                         key: {index},
-                        label: <div className="projcollaspehead">
-                                  <h2>{project[1]}</h2>
+                        label: <div className="projcollaspe-head">
+                                  
+                                  <h3 className="projname">{project[1]}</h3>
+                                  <Button type="link" icon={<CloseOutlined  style={{ fontSize: '15px',color:'red'}} onClick={()=>Deleteprojuct(project[2])}/>} />
+                                  
                                 </div>,
-                        children: <p>{project[0]}</p>,
+                        children: <div className="projcollaspe-content">
+                                  <p className="projdes">{project[0]}</p>
+                                    
+                                  <Link className="projedit-btn" to={`/myproject/${project[1]}/${project[2]}`}><FormOutlined  style={{ fontSize: '15px',color:'grey'}}/></Link>
+                                  </div>,
                       },
                       ]}
                       
                     >
                       
                     </Collapse>
-                    </ConfigProvider>
+                    
                   </div>
                   
                   
-                ))},
+                ))}
               </Card>
             </div>
           );
