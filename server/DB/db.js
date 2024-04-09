050c1728742ada3d708c9ce5b59e469709b9afab
RoongkanSangnual1 <s6304062636278@email.kmutnb.ac.th>
2024-04-09 11:15:22 +0700

    edit

--- server/DB/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const dbConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'robo',
-});
-
-module.exports = dbConnection;
