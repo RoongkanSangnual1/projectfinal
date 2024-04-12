2a6bf2d810c38dc280199fee16541086048afb4f
RoongkanSangnual1 <s6304062636278@email.kmutnb.ac.th>
2024-04-12 13:39:41 +0700

    edit

--- server/DB/db.js
+++ server/DB/db.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql2');
+
+
+
 // const dbConnection = mysql.createConnection({
-//   // host: 'mysql-server',
-//   host:'localhost',
+//   host: 'db',
+//   port: 3306,
 //   user: 'root',
-//   password: '',
+//   password: '1234',
 //   database: 'robo',
 // });
-
 // module.exports = dbConnection;const mysql = require('mysql2');
+
+
 const dbConnection = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
   user: 'root',
   password: '',
   database: 'robo',
 });
 
 module.exports = dbConnection;
-
