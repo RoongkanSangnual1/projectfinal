04a8e2c7366c23138ec5aed5c83da66de2866e44
RoongkanSangnual1 <s6304062636278@email.kmutnb.ac.th>
2024-04-13 12:31:59 +0700

    commit

--- pjfinal/src/components/Editprjectall.js
+++ pjfinal/src/components/Editprjectall.js
@@ -51,7 +51,7 @@ const EditPrjectDash = () => {
   const [owner,setOwner]= useState("")
     const user = localStorage.getItem("user");
     const tokenuser = localStorage.getItem("token");
-    const token = new URLSearchParams(location.search).get('token');
+    const token = new URLSearchParams(location.search).get('Share');
     const [header, payload,signature] = token.split('.');
     const decodedHeader = atob(header);
     const decodedPayload = JSON.parse(atob(payload));
@@ -59,7 +59,7 @@ const EditPrjectDash = () => {
 
       const fetchData = async () => {
           try {
-            const response = await axios.get(`http://127.0.0.1:5000/edit-issue?token=${token}`, {
+            const response = await axios.get(`http://127.0.0.1:5000/edit-issue?Share=${token}`, {
               headers: {
                 'Authorization': `Bearer ${tokenuser}`,
                 'Access-Control-Allow-Origin' : '*',
@@ -333,9 +333,11 @@ const handleCopy = () => {
       console.log(name_pj)
 
   return (
+<div>
+<Navbar />
 
         <div className={activeTabKey1 === 'tab5' ? 'ProjectDashh' : 'ProjectDash'}>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className='ProjectDash-Head'>
       <Link to={`/home`} className="projedit-btn" style={{ fontSize: '30px', color: '#064061', marginRight: '20px',marginTop:'20px' }}>
                              <LeftCircleOutlined />
@@ -391,6 +393,7 @@ const handleCopy = () => {
           </>
         )}
       </div>
+    </div>
     </div>
   );
   
